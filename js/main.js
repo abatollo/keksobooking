494e00df625f264a5b86550668610fc6222f8843
Aleksandr Batollo <a@batollo.ru>
2020-06-22 15:38:31 +0300

    Исправил логику работы с объектами для отрисовки — они передаются функции как аргумент. Исправил способ заполнения массива — используется метод push. Вынес константы в начало кода.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,44 @@
 'use strict';
 
+var AVATAR_LIST = [
+  'img/avatars/user01.png',
+  'img/avatars/user02.png',
+  'img/avatars/user03.png',
+  'img/avatars/user04.png',
+  'img/avatars/user05.png',
+  'img/avatars/user06.png',
+  'img/avatars/user07.png',
+  'img/avatars/user08.png'
+];
+
+var OFFER_TYPE_LIST = [
+  'palace',
+  'flat',
+  'house',
+  'bungalo'
+];
+
+var OFFER_CHECKIN_CHECKOUT_LIST = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+var OFFER_FEATURES_LIST = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+var OFFER_PHOTOS_LIST = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
+
 var getRandomArrayItem = function (arr) {
   var randomArrayItem = arr[getRandomNumber(0, arr.length)];
 
@@ -42,53 +81,13 @@ var getMapWidth = function () {
 };
 
 var generateMockAds = function (arrLength) {
-
-  var AVATAR_LIST = [
-    'img/avatars/user01.png',
-    'img/avatars/user02.png',
-    'img/avatars/user03.png',
-    'img/avatars/user04.png',
-    'img/avatars/user05.png',
-    'img/avatars/user06.png',
-    'img/avatars/user07.png',
-    'img/avatars/user08.png'
-  ];
-
-  var OFFER_TYPE_LIST = [
-    'palace',
-    'flat',
-    'house',
-    'bungalo'
-  ];
-
-  var OFFER_CHECKIN_CHECKOUT_LIST = [
-    '12:00',
-    '13:00',
-    '14:00'
-  ];
-
-  var OFFER_FEATURES_LIST = [
-    'wifi',
-    'dishwasher',
-    'parking',
-    'washer',
-    'elevator',
-    'conditioner'
-  ];
-
-  var OFFER_PHOTOS_LIST = [
-    'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-    'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
-  ];
-
   var arr = [];
 
   for (var i = 0; i < arrLength; i++) {
     var locationX = getRandomNumber(0, getMapWidth());
     var locationY = getRandomNumber(130, 630);
 
-    arr[i] = {
+    arr.push({
       author: {
         avatar: AVATAR_LIST[i]
       },
@@ -109,7 +108,7 @@ var generateMockAds = function (arrLength) {
         x: locationX,
         y: locationY
       }
-    };
+    });
   }
 
   return arr;
@@ -127,20 +126,21 @@ var renderAd = function (ad, templateContent) {
   return adElement;
 };
 
-var renderAds = function (templateId) {
+var renderAds = function (adsArray, templateId) {
   var fragment = document.createDocumentFragment();
   var pinTemplateContent = document.querySelector(templateId).content.querySelector('.map__pin');
 
-  for (var i = 0; i < ads.length; i++) {
-    fragment.appendChild(renderAd(ads[i], pinTemplateContent));
+  for (var i = 0; i < adsArray.length; i++) {
+    fragment.appendChild(renderAd(adsArray[i], pinTemplateContent));
   }
 
   document.querySelector('.map__pins').appendChild(fragment);
 };
 
 var map = document.querySelector('.map');
+
 map.classList.remove('map--faded');
 
 var ads = generateMockAds(8);
 
-renderAds('#pin');
+renderAds(ads, '#pin');
