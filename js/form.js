572358fba53e0de49433fb8198817eb4657bf586
Aleksandr Batollo <a@batollo.ru>
2020-07-23 17:41:50 +0300

    Добавил отправку формы и обработку успеха и ошибок

--- js/form.js
+++ js/form.js
@@ -17,30 +17,10 @@ window.form = (function () {
     'palace': 10000
   };
 
-  // Добавляем на select в форме фильтрации объявлений атрибут disabled, который убираем, если пользователь активировал карту и форму
-  for (var i = 0; i < window.map.mapFiltersSelects.length; i++) {
-    window.map.mapFiltersSelects[i].disabled = true;
-  }
-
-  // Добавляем на fieldset в форме фильтрации объявлений атрибут disabled, который убираем, если пользователь активировал карту и форму
-  window.map.mapFiltersFieldset.disabled = true;
-
-  // Добавляем форме класс ad-form--disabled, который убираем, если пользователь активировал карту и форму
   var adForm = document.querySelector('.ad-form');
-  adForm.classList.add('ad-form--disabled');
-
-  // Добавляем на блоки fieldset в форме заполнения объявления атрибут disabled, который убираем, если пользователь активировал карту и форму
   var adFormFieldsets = document.querySelectorAll('.ad-form fieldset');
-  for (i = 0; i < adFormFieldsets.length; i++) {
-    adFormFieldsets[i].disabled = true;
-  }
-
-  // Устанавливаем координаты середины главной метки и подставляем их в поле адреса
-  var mapPinMain = document.querySelector('.map__pin--main');
-  var addressX = mapPinMain.offsetLeft + (mapPinMain.offsetWidth / 2);
-  var addressY = mapPinMain.offsetTop + (mapPinMain.offsetHeight / 2);
   var addressField = document.querySelector('.ad-form #address');
-  addressField.value = addressX + ', ' + addressY;
+
 
   // Валидируем значение количества гостей: их должно быть не больше, чем количество комнат
   var roomNumberSelect = document.querySelector('#room_number');
@@ -48,31 +28,55 @@ window.form = (function () {
   var capacitySelect = document.querySelector('#capacity');
   var capacitySelectOptions = capacitySelect.querySelectorAll('option');
 
-  // По умолчанию в количестве комнат устаналиваем одну комнату
-  for (i = 0; i < roomNumberSelectOptions.length; i++) {
-    if (roomNumberSelectOptions[i].value === '1') {
-      roomNumberSelectOptions[i].selected = true;
+  var resetForm = function () {
+    // Добавляем на select в форме фильтрации объявлений атрибут disabled, который убираем, если пользователь активировал карту и форму
+    for (var i = 0; i < window.map.mapFiltersSelects.length; i++) {
+      window.map.mapFiltersSelects[i].disabled = true;
     }
-  }
-  // По умолчанию в количестве гостей устаналиваем одного гостя
-  for (i = 0; i < capacitySelectOptions.length; i++) {
-    if (capacitySelectOptions[i].value === '1') {
-      capacitySelectOptions[i].selected = true;
+
+    // Добавляем на fieldset в форме фильтрации объявлений атрибут disabled, который убираем, если пользователь активировал карту и форму
+    window.map.mapFiltersFieldset.disabled = true;
+
+    // Добавляем форме класс ad-form--disabled, который убираем, если пользователь активировал карту и форму
+    adForm.classList.add('ad-form--disabled');
+
+    // Добавляем на блоки fieldset в форме заполнения объявления атрибут disabled, который убираем, если пользователь активировал карту и форму
+    for (i = 0; i < adFormFieldsets.length; i++) {
+      adFormFieldsets[i].disabled = true;
     }
-  }
-  // По умолчанию блокируем возможность выбрать любой вариант, кроме «для 1 гостя»
-  for (i = 0; i < capacitySelectOptions.length; i++) {
-    if (capacitySelectOptions[i].value !== '1') {
-      capacitySelectOptions[i].disabled = true;
+
+    // Устанавливаем координаты середины главной метки и подставляем их в поле адреса
+    var mapPinMain = document.querySelector('.map__pin--main');
+    var addressX = mapPinMain.offsetLeft + (mapPinMain.offsetWidth / 2);
+    var addressY = mapPinMain.offsetTop + (mapPinMain.offsetHeight / 2);
+    addressField.value = addressX + ', ' + addressY;
+
+    // По умолчанию в количестве комнат устаналиваем одну комнату
+    for (i = 0; i < roomNumberSelectOptions.length; i++) {
+      if (roomNumberSelectOptions[i].value === '1') {
+        roomNumberSelectOptions[i].selected = true;
+      }
     }
-  }
+    // По умолчанию в количестве гостей устаналиваем одного гостя
+    for (i = 0; i < capacitySelectOptions.length; i++) {
+      if (capacitySelectOptions[i].value === '1') {
+        capacitySelectOptions[i].selected = true;
+      }
+    }
+    // По умолчанию блокируем возможность выбрать любой вариант, кроме «для 1 гостя»
+    for (i = 0; i < capacitySelectOptions.length; i++) {
+      if (capacitySelectOptions[i].value !== '1') {
+        capacitySelectOptions[i].disabled = true;
+      }
+    }
+  };
 
   // При выборе другого количества комнат — смотрим сколько комнат выбрано — перебираем все возможные числа гостей —
   // — разрешаем выбирать только те числа, которые подходят, — все остальные числа отключаем
   roomNumberSelect.addEventListener('change', function () {
     // Перебириаем массив с колличеством мест для гостей. Если значение опции получается найти в массиве (то есть индекс элемента >= 0),
     // который является значением соответствующего свойства в объекте COMPLIANCE_OPTIONS, то опцию надо включить, а если нет — выключить
-    for (i = 0; i < capacitySelectOptions.length; i++) {
+    for (var i = 0; i < capacitySelectOptions.length; i++) {
       if (COMPLIANCE_OPTIONS[roomNumberSelect.value].indexOf(capacitySelectOptions[i].value) >= 0) {
         capacitySelectOptions[i].disabled = false;
       } else {
@@ -103,9 +107,86 @@ window.form = (function () {
     timeInSelect.value = timeOutSelect.value;
   });
 
+  var form = document.querySelector('.ad-form');
+  form.addEventListener('submit', function (evt) {
+    window.upload(new FormData(form), successHandler, errorHandler);
+    evt.preventDefault();
+  });
+
+  var successHandler = function () {
+    window.map.deactivateMap();
+    resetForm();
+    adForm.reset();
+    window.map.mapPinMain.style.left = window.util.PIN_LEFT_DEFAULT_POSITION;
+    window.map.mapPinMain.style.top = window.util.PIN_TOP_DEFAULT_POSITION;
+    window.move.newAdress();
+    renderSuccess(window.util.SUCCESS_ID);
+  };
+
+  var renderSuccess = function (templateId) {
+    var fragment = document.createDocumentFragment();
+    var successTemplateContent = document.querySelector(templateId).content.querySelector('.success');
+    var successElement = successTemplateContent.cloneNode(true);
+    fragment.appendChild(successElement);
+    document.querySelector('main').appendChild(fragment);
+
+    successElement.addEventListener('click', function () {
+      successElement.remove();
+      document.removeEventListener('keydown', onSuccessEscPress);
+    });
+    document.addEventListener('keydown', onSuccessEscPress);
+  };
+
+  var onSuccessEscPress = function (evt) {
+    if (evt.keyCode === window.util.ESC_KEYCODE) {
+      var successElement = document.querySelector('.success');
+      successElement.remove();
+      document.removeEventListener('keydown', onSuccessEscPress);
+    }
+  };
+
+  var renderError = function (templateId) {
+    var fragment = document.createDocumentFragment();
+    var errorTemplateContent = document.querySelector(templateId).content.querySelector('.error');
+    var errorElement = errorTemplateContent.cloneNode(true);
+    fragment.appendChild(errorElement);
+    document.querySelector('main').appendChild(fragment);
+
+    errorElement.addEventListener('click', function () {
+      errorElement.remove();
+      document.removeEventListener('keydown', onErrorEscPress);
+    });
+    document.addEventListener('keydown', onErrorEscPress);
+  };
+
+  var onErrorEscPress = function (evt) {
+    if (evt.keyCode === window.util.ESC_KEYCODE) {
+      var errorElement = document.querySelector('.error');
+      errorElement.remove();
+      document.removeEventListener('keydown', onErrorEscPress);
+    }
+  };
+
+  var errorHandler = function () {
+    renderError(window.util.ERROR_ID);
+  };
+
+  var resetBtn = document.querySelector('.ad-form__reset');
+
+  resetBtn.addEventListener('click', function () {
+    adForm.reset();
+  });
+
+  resetBtn.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === window.util.ENTER_KEYCODE) {
+      adForm.reset();
+    }
+  });
+
   return {
     adForm: adForm,
     adFormFieldsets: adFormFieldsets,
-    addressField: addressField
+    addressField: addressField,
+    resetForm: resetForm
   };
 })();
--- js/map.js
+++ js/map.js
@@ -6,11 +6,13 @@ window.map = (function () {
   var mapFiltersSelects = document.querySelectorAll('.map__filters select');
   var mapFiltersFieldset = document.querySelector('.map__filters fieldset');
 
+  var map = document.querySelector('.map');
+  var mapPinMain = document.querySelector('.map__pin--main');
+
   // Функция активации карты по клику или нажатию Enter на главном маркере
   var activateMap = function () {
 
     // Убираем затенение у карты
-    var map = document.querySelector('.map');
     map.classList.remove('map--faded');
 
     // Удаляем обработчики клика мышкой и нажатия с клавиатуры по главному маркеру, раз пользователь активировал карту и форму
@@ -36,6 +38,37 @@ window.map = (function () {
     window.load.getData(window.data.successHandler, window.data.errorHandler);
   };
 
+  var deactivateMap = function () {
+    // Добавляем затенение карте
+    map.classList.add('map--faded');
+
+    // Убираем у карты стиль неактивного состояния по нажатию на главный маркер мышью или с клавиатуры
+    mapPinMain.addEventListener('mousedown', onMapPinMainClick);
+    mapPinMain.addEventListener('keydown', onMapPinMainKeydown);
+
+    // Добавляем select в форме фильтрации объявлений атрибут disabled, раз пользователь деактивировал карту и форму
+    for (var i = 0; i < mapFiltersSelects.length; i++) {
+      mapFiltersSelects[i].disabled = true;
+    }
+
+    // Добавляем fieldset в форме фильтрации объявлений атрибут disabled, раз пользователь деактивировал карту и форму
+    mapFiltersFieldset.disabled = true;
+
+    // Добавляем форме класс ad-form--disabled, раз пользователь деактивировал карту и форму
+    window.form.adForm.classList.add('ad-form--disabled');
+
+    // Добавляем блокам fieldset в форме заполнения объявления атрибут disabled, раз пользователь деактивировал карту и форму
+    for (i = 0; i < window.form.adFormFieldsets.length; i++) {
+      window.form.adFormFieldsets[i].disabled = true;
+    }
+
+    // Удаляем все метки, кроме главной
+    var allPins = document.querySelectorAll('.map__pin:not(.map__pin--main)');
+    for (i = 0; i < allPins.length; i++) {
+      allPins[i].remove();
+    }
+  };
+
   // Функция реакции на нажатие мышкой на главный маркер
   var onMapPinMainClick = function (evt) {
     evt.preventDefault();
@@ -54,12 +87,13 @@ window.map = (function () {
   };
 
   // Убираем у карты стиль неактивного состояния по нажатию на главный маркер мышью или с клавиатуры
-  var mapPinMain = document.querySelector('.map__pin--main');
   mapPinMain.addEventListener('mousedown', onMapPinMainClick);
   mapPinMain.addEventListener('keydown', onMapPinMainKeydown);
 
   return {
     mapFiltersSelects: mapFiltersSelects,
-    mapFiltersFieldset: mapFiltersFieldset
+    mapFiltersFieldset: mapFiltersFieldset,
+    deactivateMap: deactivateMap,
+    mapPinMain: mapPinMain
   };
 })();
